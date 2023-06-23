48296277ad416fb166195ab1268ea8706632aecd
Elvis O <elvis.osujic@gmail.com>
2023-06-23 15:46:15 +0100

    Auth guards

--- src/routes/team/editTeam.ts
+++ src/routes/team/editTeam.ts
@@ -3,6 +3,7 @@ import { body, validationResult } from "express-validator";
 import { NotFoundError, validateRequest } from "@ticketifyorg/common";
 import { Team } from "../../../model/teams";
 import { authMiddleware } from "../../middlewares/auth";
+import { adminGuard } from "../../middlewares/adminGuard";
 
 const router = express.Router();
 
@@ -20,6 +21,7 @@ router.put(
   ],
   validateRequest,
   authMiddleware,
+  adminGuard,
   async (req: Request, res: Response) => {
     const { name, league, stadium, yearFounded, status } = req.body;
 
